25135d63ea3d24462bcab9c29cf22803685d7ffa
Kaiyin Zhong <kindlychung@gmail.com>
2016-01-18 03:54:15 +0100

    first commit

---
+++ swiftsci/main.swift
@@ -0,0 +1,69 @@
+//
+//  main.swift
+//  swiftsci
+//
+//  Created by Kaiyin Zhong on 1/17/16.
+//  Copyright (c) 2016 emc. All rights reserved.
+//
+
+import Foundation
+import AppKit
+import Cocoa
+
+
+
+
+
+let doc: String = "Not a serious example.\n" +
+        "\n" +
+        "Usage:\n" +
+        "  swiftsci sci \n" +
+//        "  swiftsci <function> <value> [( , <value> )]...\n" +
+//        "  swiftsci <value> ( ( + | - | * | / ) <value> )...\n" +
+        "  swiftsci (-h | --help)\n" +
+        "\n" +
+//        "Examples:\n" +
+//        "  swiftsci 1 + 2 + 3 + 4 + 5\n" +
+//        "  swiftsci 1 + 2 '*' 3 / 4 - 5    # note quotes around '*'\n" +
+//        "  swiftsci sum 10 , 20 , 30 , 40\n" +
+        "Options:\n" +
+        "  -h, --help\n"
+
+var args = Process.arguments
+args.removeAtIndex(0) // arguments[0] is always the program_name
+let opts = Docopt.parse(doc, argv: args, help: true, version: "1.0")
+
+//func showNotification(title: String, info: String) -> Void {
+//    let notification = NSUserNotification()
+//    notification.title = title
+//    notification.informativeText = info
+//    notification.soundName = NSUserNotificationDefaultSoundName
+//    NSUserNotificationCenter.defaultUserNotificationCenter().deliverNotification(notification)
+//}
+
+let pasteBoard = NSPasteboard.generalPasteboard()
+if let sumCmd = opts["sci"] {
+    func replaceClipSci() throws {
+        let o = pasteBoard.pasteboardItems
+        if let o1 = o {
+            let oldString = o1[0].stringForType("public.utf8-plain-text")!
+            let newString = try oldString.sci()
+            if newString != oldString {
+                pasteBoard.clearContents()
+                pasteBoard.writeObjects([newString])
+                print("Old: \(oldString)\nNew: \(newString)")
+//                showNotification("Clipboard changed", info: "Old: \(oldString.firstN(10))\nNew: \(newString.firstN(10))")
+            }
+        }
+    }
+    while true {
+        try replaceClipSci()
+        NSThread.sleepForTimeInterval(0.1)
+    }
+}
+
+
+
+
+//let z1 = try "Here we have -3.213e-043, and there we have 4.243E+432".sci()
+//print(z1)
---
+++ swiftsci/sciNotation/sci.swift
@@ -0,0 +1,102 @@
+//
+// Created by Kaiyin Zhong on 1/18/16.
+// Copyright (c) 2016 emc. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+
+func head(s: String) -> Character {
+    return s[s.startIndex]
+}
+
+func tail(s: String) -> String {
+    let range = (s.startIndex.advancedBy(1)) ..< s.endIndex
+    return s[range]
+}
+
+
+
+
+func dealWithSign(s: String) -> String {
+    if head(s) == "-" {
+        return "-\(dealWithSign(tail(s)))"
+    } else if head(s) == "+" {
+        return dealWithSign(tail(s))
+    } else {
+        var s1 = s
+        while (head(s1) == "0") {
+            s1 = tail(s1)
+        }
+        return s1
+    }
+}
+
+
+let superscriptDict: [Character:Character] = [
+        "0": "\u{2070}",
+        "1": "\u{00b9}",
+        "2": "\u{00b2}",
+        "3": "\u{00b3}",
+        "4": "\u{2074}",
+        "5": "\u{2075}",
+        "6": "\u{2076}",
+        "7": "\u{2077}",
+        "8": "\u{2078}",
+        "9": "\u{2079}",
+        "-": "⁻"
+]
+
+public extension String {
+    public func rangeFromNSRange(aRange: NSRange) -> Range<String.Index> {
+        let s = self.startIndex.advancedBy(aRange.location)
+        let e = self.startIndex.advancedBy(aRange.location + aRange.length)
+        return s..<e
+    }
+
+    public func firstN(n: Int) -> String {
+        return self.substringToIndex(self.startIndex.advancedBy(n))
+    }
+    public var ns : NSString {return self as NSString}
+    public subscript (aRange: NSRange) -> String? {
+        get {return self.substringWithRange(self.rangeFromNSRange(aRange))}
+    }
+
+    public var cdr: String {return isEmpty ? "" : String(characters.dropFirst())}
+
+    public func toCamel() throws -> String {
+        var s = self
+        let regex = try NSRegularExpression(pattern: "_+(.)", options: [])
+        let matches = regex.matchesInString(s, options:[], range:NSMakeRange(0, s.ns.length)).reverse()
+        for match in matches {
+//            print("match = \(s[match.range]!)")
+            let matchRange = s.rangeFromNSRange(match.range) // the whole match range
+            let replaceRange = match.rangeAtIndex(1)         // range of the capture group
+            let uc = s[replaceRange]!.uppercaseString
+            print(uc)
+            s.replaceRange(matchRange, with: uc)
+        }
+        if s.hasPrefix("_") {s = s.cdr}
+        return s
+    }
+
+    public func sci() throws -> String {
+        var s = self
+        let regex = try NSRegularExpression(pattern: "([+-]?\\d+(\\.\\d+)?)[Ee]([+-]?\\d+)", options: [])
+        let matches = regex.matchesInString(s, options: [], range: NSMakeRange(0, s.ns.length)).reverse()
+        for match in matches {
+            let matchRange = s.rangeFromNSRange(match.range)
+            let coefRange = match.rangeAtIndex(1)
+            let exponentRange = match.rangeAtIndex(3)
+            let coef = s[coefRange]!
+            let exponent = dealWithSign(s[exponentRange]!).characters.map({
+                (x: Character) -> Character in
+                superscriptDict[x]!
+            })
+            let newSci = "\(coef)\u{00d7}10\(String(exponent))"
+            s.replaceRange(matchRange, with: newSci)
+        }
+        return s
+    }
+}
