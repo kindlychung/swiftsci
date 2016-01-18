72b4a3511e473fab65fea90c658b21ae3b86e880
Kaiyin Zhong <kindlychung@gmail.com>
2016-01-18 16:20:36 +0100

    strip ibook nonsense

--- swiftsci/main.swift
+++ swiftsci/main.swift
@@ -10,13 +10,14 @@ import Foundation
 import AppKit
 import Cocoa
 
-print("Starting...")
+print("Starting...1")
 let doc: String = "Not a serious example.\n" +
         "\n" +
         "Usage:\n" +
-        "  swiftsci sci \n" +
-        "  swiftsci ibook \n" +
+//        "  swiftsci sci \n" +
+//        "  swiftsci ibook \n" +
 //        "  swiftsci <function> <value> [( , <value> )]...\n" +
+        "  swiftsci <function> \n" +
 //        "  swiftsci <value> ( ( + | - | * | / ) <value> )...\n" +
         "  swiftsci (-h | --help)\n" +
         "\n" +
@@ -31,35 +32,49 @@ var args = Process.arguments
 args.removeAtIndex(0) // arguments[0] is always the program_name
 let opts = Docopt.parse(doc, argv: args, help: true, version: "1.0")
 
-//func showNotification(title: String, info: String) -> Void {
-//    let notification = NSUserNotification()
-//    notification.title = title
-//    notification.informativeText = info
-//    notification.soundName = NSUserNotificationDefaultSoundName
-//    NSUserNotificationCenter.defaultUserNotificationCenter().deliverNotification(notification)
-//}
-
 let pasteBoard = NSPasteboard.generalPasteboard()
 if let sumCmd = opts["sci"] {
-    func replaceClipSci() throws {
+}
+
+let funcMap = [
+        "sci": {
+            (s: String) throws -> String in try s.sci()
+        },
+        "ibook": {
+            (s: String) throws -> String in try s.ibook()
+        },
+]
+
+func replaceClip(f: String throws -> String) throws {
+    func helper() throws {
         let o = pasteBoard.pasteboardItems
         if let o1 = o {
             let oldString = o1[0].stringForType("public.utf8-plain-text")!
-            let newString = try oldString.sci1()
+//        let newString = try oldString.sci()
+            let newString = try f(oldString)
             if newString != oldString {
                 print("Hit!")
                 pasteBoard.clearContents()
                 pasteBoard.writeObjects([newString])
                 print("Old: \(oldString)\nNew: \(newString)")
-//                showNotification("Clipboard changed", info: "Old: \(oldString.firstN(10))\nNew: \(newString.firstN(10))")
             }
         }
     }
+
     while true {
-        try replaceClipSci()
+        try helper()
         NSThread.sleepForTimeInterval(0.1)
     }
 }
+
+if let cmd = opts["<function>"] {
+    if let f = funcMap[cmd as! String] {
+        try replaceClip(f)
+    }
+}
+
+
+
 
 
 
--- swiftsci/sciNotation/sci.swift
+++ swiftsci/sciNotation/sci.swift
@@ -17,8 +17,6 @@ func tail(s: String) -> String {
 }
 
 
-
-
 func dealWithSign(s: String) -> String {
     if head(s) == "-" {
         return "-\(dealWithSign(tail(s)))"
@@ -35,72 +33,61 @@ func dealWithSign(s: String) -> String {
 
 
 let superscriptDict: [Character:Character] = [
-    "0": "\u{2070}",
-    "1": "\u{00b9}",
-    "2": "\u{00b2}",
-    "3": "\u{00b3}",
-    "4": "\u{2074}",
-    "5": "\u{2075}",
-    "6": "\u{2076}",
-    "7": "\u{2077}",
-    "8": "\u{2078}",
-    "9": "\u{2079}",
-    "-": "⁻"
+        "0": "\u{2070}",
+        "1": "\u{00b9}",
+        "2": "\u{00b2}",
+        "3": "\u{00b3}",
+        "4": "\u{2074}",
+        "5": "\u{2075}",
+        "6": "\u{2076}",
+        "7": "\u{2077}",
+        "8": "\u{2078}",
+        "9": "\u{2079}",
+        "-": "⁻"
 ]
 
 public extension String {
     public func rangeFromNSRange(aRange: NSRange) -> Range<String.Index> {
         let s = self.startIndex.advancedBy(aRange.location)
         let e = self.startIndex.advancedBy(aRange.location + aRange.length)
-        return s..<e
+        return s ..< e
     }
-    
+
     public func firstN(n: Int) -> String {
         return self.substringToIndex(self.startIndex.advancedBy(n))
     }
-    public var ns : NSString {return self as NSString}
-    public subscript (aRange: NSRange) -> String? {
-        get {return self.substringWithRange(self.rangeFromNSRange(aRange))}
+    public var ns: NSString {
+        return self as NSString
     }
-    
-    public var cdr: String {return isEmpty ? "" : String(characters.dropFirst())}
-    
-    public func toCamel() throws -> String {
-        var s = self
-        let regex = try NSRegularExpression(pattern: "_+(.)", options: [])
-        let matches = regex.matchesInString(s, options:[], range:NSMakeRange(0, s.ns.length)).reverse()
-        for match in matches {
-            //            print("match = \(s[match.range]!)")
-            let matchRange = s.rangeFromNSRange(match.range) // the whole match range
-            let replaceRange = match.rangeAtIndex(1)         // range of the capture group
-            let uc = s[replaceRange]!.uppercaseString
-            print(uc)
-            s.replaceRange(matchRange, with: uc)
+    public subscript(aRange: NSRange) -> String? {
+        get {
+            return self.substringWithRange(self.rangeFromNSRange(aRange))
         }
-        if s.hasPrefix("_") {s = s.cdr}
-        return s
     }
-    
+
+    public var cdr: String {
+        return isEmpty ? "" : String(characters.dropFirst())
+    }
+
+    private func removeByRegex(s: String, match: NSTextCheckingResult) -> String {
+        return ""
+    }
+
+    public func ibook() throws -> String {
+        return try replaceByRegex("(“|”|\\s+Excerpt From:[^“”]+“[^“”]+” iBooks.)", f: removeByRegex)
+    }
+
+    public func toCaml() throws -> String {
+        func f(s: String, match: NSTextCheckingResult) -> String {
+            return s[
+            match.rangeAtIndex(1)         // range of the capture group
+            ]!.uppercaseString
+        }
+
+        return try replaceByRegex("_+(.)", f: f)
+    }
+
     public func sci() throws -> String {
-        var s = self
-        let regex = try NSRegularExpression(pattern: "([+-]?\\d+(\\.\\d+)?)[Ee]([+-]?\\d+)", options: [])
-        let matches = regex.matchesInString(s, options: [], range: NSMakeRange(0, s.ns.length)).reverse()
-        for match in matches {
-            let matchRange = s.rangeFromNSRange(match.range)
-            let coefRange = match.rangeAtIndex(1)
-            let exponentRange = match.rangeAtIndex(3)
-            let coef = s[coefRange]!
-            let exponent = dealWithSign(s[exponentRange]!).characters.map({
-                (x: Character) -> Character in
-                superscriptDict[x]!
-            })
-            let newSci = "\(coef)\u{00d7}10\(String(exponent))"
-            s.replaceRange(matchRange, with: newSci)
-        }
-        return s
-    }
-    
-    public func sci1() throws -> String {
         func f(string: String, match: NSTextCheckingResult) -> String {
             let coef = string[match.rangeAtIndex(1)]!
             let exponent = dealWithSign(string[match.rangeAtIndex(3)]!).characters.map({
@@ -108,13 +95,14 @@ public extension String {
             })
             return "\(coef)\u{00d7}10\(String(exponent))"
         }
+
         return try replaceByRegex("([+-]?\\d+(\\.\\d+)?)[Ee]([+-]?\\d+)", f: f)
     }
-    
-    public func replaceByRegex(pattern: String, regexOptions: NSRegularExpressionOptions = [], matchOptions: NSMatchingOptions = [], f: (string: String, match: NSTextCheckingResult) -> String) throws -> String {
+
+    public func replaceByRegex(pattern: String, regexOptions: NSRegularExpressionOptions = [], matchOptions: NSMatchingOptions = [], f: (string:String, match:NSTextCheckingResult) -> String) throws -> String {
         var s = self
         let regex = try NSRegularExpression(pattern: pattern, options: regexOptions)
-        let matches = regex.matchesInString(s , options: matchOptions, range: NSMakeRange(0, s.ns.length)).reverse()
+        let matches = regex.matchesInString(s, options: matchOptions, range: NSMakeRange(0, s.ns.length)).reverse()
         for match in matches {
             let matchRange = s.rangeFromNSRange(match.range)
             let newString = f(string: s, match: match)
